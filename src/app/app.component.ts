13f20ca35d16ffea896ba25ef2ae48c2901062cb
Mauricio Guzmán Salazar <mauricio.guzman.salazar@gmail.com>
2018-04-16 18:12:55 -0500

    changes colors according to markup file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,7 +45,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.backgroundColorByHexString("#03b6b3");
+      this.statusBar.backgroundColorByHexString("#00919A");
       this.splashScreen.hide();
     });
   }
